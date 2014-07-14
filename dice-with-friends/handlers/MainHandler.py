1faf5114ea519e7bb38db3bc392254f99fdf6a8a
David Fisher <fisherds@rose-hulman.edu>
2014-07-14 00:54:12 -0400

    Add display name
    Added a mechanism to add a display name.  There are still of lot of
    details that will be refactored into a BaseHandler.

--- dice-with-friends/handlers/MainHandler.py
+++ dice-with-friends/handlers/MainHandler.py
@@ -1,15 +1,36 @@
 
-from google.appengine.ext import ndb
+from google.appengine.api import users
 import webapp2
+import logging
 
 import main
+import models
+import time
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        template = main.jinja_env.get_template("templates/play.html")
-        self.response.out.write(template.render({'placeholder': None}))
+        user = users.get_current_user()
+        if not user:
+            template = main.jinja_env.get_template("templates/not_logged_in.html")
+            self.response.out.write(template.render({'login_url': users.create_login_url("/")}))
+        else:
+            player = models.Player.get_player_from_email(user.email())
+            if not player.display_name or not len(player.display_name) > 0:
+              template = main.jinja_env.get_template("templates/set_display_name.html")
+              self.response.out.write(template.render({'logout_url': users.create_logout_url("/")}))
+            else:
+              template = main.jinja_env.get_template("templates/main.html")
+              self.response.out.write(template.render({'player': player,
+                                                     'logout_url': users.create_logout_url("/")}))
 
     def post(self):
-        """ Receives the updated round scores from a player after they complete a round. """
+        """ Used to set the display name for a player. """
+        user = users.get_current_user()
+        if not user:
+          raise Exception("Missing user.")
+        player = models.Player.get_player_from_email(user.email())
+        player.display_name = self.request.get('display_name')
+        player.put()
+        time.sleep(0.5) # Hack.  Didn't want to implement a Parent key to get strong consistency.
         self.redirect(self.request.referer)
---
+++ dice-with-friends/models.py
@@ -0,0 +1,49 @@
+from google.appengine.ext import ndb
+import logging
+
+class Player(ndb.Model):
+  """ Holder information for a player. """
+  lowercase_email = ndb.StringProperty(required=True)
+  display_name = ndb.StringProperty()
+  create_time = ndb.DateTimeProperty(auto_now_add=True)
+  last_update = ndb.DateTimeProperty(auto_now=True)
+
+  def get_display_name(self):
+    """Returns a suitable display name for use on the leaderboards."""
+    if self.display_name:
+      return self.display_name
+    elif self.lowercase_email:
+      return self.lowercase_emailx
+    else:
+      return ''
+
+  @staticmethod
+  def get_player_from_email(email):
+    """Helper method to get the Player object corresponding to the given User.
+    Creates a new Player object if one didn't exist already.
+    """
+    player = Player.query(Player.lowercase_email==email.lower()).get()
+    if not player:
+      logging.info("Failed to find player by userid, creating new user")
+      player = Player(lowercase_email=email.lower())
+      player.put()
+    logging.info("Player: " + player.key.urlsafe())
+    return player
+
+class Game(ndb.Model):
+    """ Dice with Friends game. """
+    player1_key = ndb.KeyProperty(kind=Player)  # Created the game
+    player2_key = ndb.KeyProperty(kind=Player)  # Invited to play in the game
+    player1_scores = ndb.IntegerProperty(repeated=True)
+    player2_scores = ndb.IntegerProperty(repeated=True)
+    last_update_by_player1 = ndb.DateTimeProperty()
+    last_update_by_player2 = ndb.DateTimeProperty()
+    create_time = ndb.DateTimeProperty(auto_now_add=True)
+
+    # Convenience fields (things that could be calculated from other data.
+    rounds_completed = ndb.IntegerProperty(default=0)
+    player1_score = ndb.IntegerProperty()  # Score after rounds_completed
+    player2_score = ndb.IntegerProperty()  # Score after rounds_completed
+    winner = ndb.KeyProperty(kind=Player)
+
+

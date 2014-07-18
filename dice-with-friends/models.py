59e684f3f128ab111e3740c8010a6ee792205e3f
Matt Boutell <boutell@gmail.com>
2014-07-18 16:45:15 -0400

    Made endpoints-ready, simplified times, added isComplete

--- dice-with-friends/models.py
+++ dice-with-friends/models.py
@@ -1,9 +1,12 @@
 from google.appengine.ext import ndb
 
-class Player(ndb.Model):
+from endpoints_proto_datastore.ndb.model import EndpointsModel
+
+
+class Player(EndpointsModel):
   """ Holder information for a player. """
+  _message_fields_schema = ("id", "display_name", "create_time")
   display_name = ndb.StringProperty()
-  create_time = ndb.DateTimeProperty(auto_now_add=True)
 
   def get_name(self):
     """Returns a suitable display name for use on the leaderboards."""
@@ -11,12 +14,13 @@ class Player(ndb.Model):
       return self.display_name
     return self.key.string_id()
 
-class Game(ndb.Model):
+class Game(EndpointsModel):
   """ Dice with Friends game. """
+  _message_fields_schema = ("entityKey", "creator_key", "invitee_key", "creator_scores", "invitee_scores", 
+                            "last_touch_date_time")
   creator_key = ndb.KeyProperty(kind=Player)
   invitee_key = ndb.KeyProperty(kind=Player)
   creator_scores = ndb.IntegerProperty(repeated=True)
   invitee_scores = ndb.IntegerProperty(repeated=True)
-  last_update_by_creator = ndb.DateTimeProperty()
-  last_update_by_invitee = ndb.DateTimeProperty()
-  create_time = ndb.DateTimeProperty(auto_now_add=True)
+  last_touch_date_time = ndb.DateTimeProperty(auto_now=True)
+  is_complete = ndb.BooleanProperty()
